8b3e6a67d1860276c6b38c3b80421ae474441476
wangxi <ioswangxi@163.com>
2019-04-27 10:47:04 +0800

    修改vue-loader

--- template/build/vue-loader.conf.js
+++ template/build/vue-loader.conf.js
@@ -1,28 +1,28 @@
-'use strict';
-const utils = require('./utils');
-const config = require('../config');
-const isProduction = process.env.NODE_ENV === 'production';
+'use strict'
+const utils = require('./utils')
+const config = require('../config')
 const px2rem = require('postcss-plugin-px2rem');
 const autoprefixer = require('autoprefixer');
-const buildUser = require('../build-user');
+const buildUser = require('../build-user/config');
+const isProduction = process.env.NODE_ENV === 'production'
 const sourceMapEnabled = isProduction
-    ? config.build.productionSourceMap
-    : config.dev.cssSourceMap;
+  ? config.build.productionSourceMap
+  : config.dev.cssSourceMap
+
 const postcssData = {};
 var currPostcssData = '';
 postcssData.ok = [
     autoprefixer({
         browsers: ['iOS >= 7', 'Android > 4.0']
     }),
-    px2rem(buildUser.px2rem())
+    px2rem(buildUser.px2rem)
 ];
 postcssData.no = [
     autoprefixer({
         browsers: ['iOS >= 7', 'Android > 4.0']
     })
 ];
-currPostcssData = buildUser.px2rem() === null ? postcssData.no : postcssData.ok;
-
+currPostcssData = buildUser.px2rem === null ? postcssData.no : postcssData.ok;
 module.exports = {
     loaders: utils.cssLoaders({
         sourceMap: sourceMapEnabled,
